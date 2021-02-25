c741e9cf8e110e7165ec970f57ba33ae0591329a
aquilla <aquilla.vdberg@gmail.com>
2021-02-25 18:07:33 +0100

    fragment container view via layout editor doesn't work, now commented

--- app/src/main/java/com/example/uitest/MainActivity.java
+++ app/src/main/java/com/example/uitest/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.uitest;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
+import android.app.FragmentManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,39 +21,23 @@ public class MainActivity extends AppCompatActivity {
     TextView number2;
     int n2 = 0;
 
-    //        FragmentManager supportFragmentManager = True;
-//        {
-//            NavHostFragment navHostFragment;
-//            navHostFragment = (NavHostFragment) supportFragmentManager.findFragmentById(R.id.Fragment0);
-//            NavController navController = navHostFragment.getNavController();
-//        }
-//
-//    public void FragmentActivity(FragmentManager supportFragmentManager) {
-//        this.supportFragmentManager = supportFragmentManager;
-//    }
-
-//button.setOnClickListener(new View.OnClickListener() {
-//        @Override
-//        public void onClick(View view) {
-//            Navigation.findNavController(view).navigate(R.id.navigationAction);
-//        }
-//    })
-
-
-//public void navigate (int resId){}
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
+
         this.number1 = findViewById(R.id.textView1);
         this.number1.setText("I'm text");
+
         this.number2 = findViewById(R.id.textView2);
         this.number2.setText("I'm text too");
+
         this.button = findViewById(R.id.testButton);
         this.button.setText("I'm not clicked yet");
+
         this.button2 = findViewById(R.id.button2);
         this.button2.setText("I'm not clicked yet 2");
+
         this.button2.setOnClickListener(v -> {
             button2.setText("I'm clicked 2");
             n2 = n2 + 1;
@@ -61,23 +46,19 @@ public class MainActivity extends AppCompatActivity {
 
         this.button3 = findViewById(R.id.button3);
         this.button3.setText("Click me to navigate to the next page");
+
 //        this.button3.setOnClickListener(Navigation.createNavigateOnClickListener(R.id.Fragment1, null));
-        this.button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Navigation.findNavController(view).navigate(R.id.Fragment1);
-            }
-        });
-
+//        this.button3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//            Navigation.findNavController(button3).navigate(R.id.Fragment1);
+//            }
+//        });
     }
-
 
     public void onButtonClicked(View view) {
     this.button.setText("I'm clicked");
     n1 = n1 + 1;
     this.number1.setText("I'm clicked " +n1 + " times");
     }
-
-
-
 }
