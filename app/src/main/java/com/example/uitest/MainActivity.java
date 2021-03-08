ab13149c80098003654857dc47c13b946f07e04d
aquilla <aquilla.vdberg@gmail.com>
2021-03-08 13:47:12 +0100

    new view

--- app/src/main/java/com/example/uitest/MainActivity.java
+++ app/src/main/java/com/example/uitest/MainActivity.java
@@ -6,6 +6,11 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.NavHost;
+import androidx.navigation.NavHostController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 public class MainActivity extends AppCompatActivity {
     Button button;
@@ -20,6 +25,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout);
+
+        Navigation.findNavController(View);
+
+        NavHostFragment navHostFragment =
+                (NavHostFragment) supportFragmentManager.findFragmentById(R.id.nav_host_fragment);
+        NavController navController = navHostFragment.getNavController();
 
         this.number1 = findViewById(R.id.textView1);
         this.number1.setText("I'm text");
@@ -41,7 +52,14 @@ public class MainActivity extends AppCompatActivity {
 
         this.button3 = findViewById(R.id.button3);
         this.button3.setText("Click me to navigate to the next page");
-//        this.button3.setOnClickListener();
+
+//        this.button3.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Navigation.findNavController(view).navigate(R.id.action1);
+//            }
+//        });
+
     }
 
     public void onButtonClicked(View view) {
